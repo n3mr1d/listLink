44cf6694085b61b97e7e6efce1f583d0fc10d081
Nameraid <n3mr1d@gmail.com>
2026-02-14 20:25:06 +0700

    add migration for links

---
+++ database/migrations/2026_02_14_112415_create_links_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('cascade');
+            $table->text('description')->default('we dont have a description');
+            $table->string('url');
+            $table->string('slug')->unique();
+            $table->string('status')->default('active');
+            $table->string('category')->default('other');
+            $table->dateTime('last_check')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('links');
+    }
+};
