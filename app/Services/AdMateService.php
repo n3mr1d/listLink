f8e0daa31c9ac9dbba3f0d2753f3dbc57c65e1a9
Nameraid <n3mr1d@gmail.com>
2026-04-20 22:45:41 +0700

    firs code readme.md

--- app/Services/AdMateService.php
+++ app/Services/AdMateService.php
@@ -17,10 +17,8 @@ class AdMateService
         return Cache::remember('admate_banners_data', 3600, function () {
             try {
                 $proxy = config('crawler.proxy', 'socks5h://127.0.0.1:9050');
-                $url = 'http://admate3tczgp6digew7jpzcosq52rs7anru53imwqimron27emq7dbqd.onion/api/get-banner/s4bSEp2XFUpCAA4o/type/468-60/count/10/' . window_hostname_placeholder();
-
-                // Append domain if possible, but for server-side we'll just use the base URL
-                $baseUrl = 'http://admate3tczgp6digew7jpzcosq52rs7anru53imwqimron27emq7dbqd.onion/api/get-banner/s4bSEp2XFUpCAA4o/type/468-60/count/10';
+                $host = request()->getHost();
+                $url = "http://admate3tczgp6digew7jpzcosq52rs7anru53imwqimron27emq7dbqd.onion/api/get-banner/s4bSEp2XFUpCAA4o/type/468-60/count/10/{$host}";
                 
                 $response = Http::withOptions([
                     'proxy' => $proxy,
@@ -29,7 +27,7 @@ class AdMateService
                     ],
                     'timeout' => 20,
                     'verify' => false,
-                ])->get($baseUrl);
+                ])->get($url);
 
                 if ($response->successful()) {
                     $data = $response->json();
